0dfbd93c31334c28da4f3e889f39df666ec5eac8
musikisomorphie <wujiqing9@gmail.com>
2021-09-09 17:11:09 +0200

    add scrc bash file

--- run_scrc.sh
+++ run_scrc.sh
@@ -1,21 +1,60 @@
 #! /bin/bash
+python examples/run_expt.py --dataset scrc --algorithm deepCORAL --root_dir /home/histopath/Data/SCRC_nuclei/ --split_scheme 012 --log_dir ./logs_deepCORAL_scrc_012_0 
+python examples/run_expt.py --dataset scrc --algorithm groupDRO --root_dir /home/histopath/Data/SCRC_nuclei/ --split_scheme 012 --log_dir ./logs_groupDRO_scrc_012_0
+python examples/run_expt.py --dataset scrc --algorithm IRM --root_dir /home/histopath/Data/SCRC_nuclei/ --split_scheme 012 --log_dir ./logs_IRM_scrc_012_0
+python examples/run_expt.py --dataset scrc --algorithm ERM --root_dir /home/histopath/Data/SCRC_nuclei/ --split_scheme 012 --log_dir ./logs_ERM_scrc_012_0
 
-python examples/run_expt.py --dataset scrc --algorithm deepCORAL --root_dir /home/histopath/Data/SCRC_nuclei/ --log_dir ./logs_deepCORAL_scrc_0
-python examples/run_expt.py --dataset scrc --algorithm groupDRO --root_dir /home/histopath/Data/SCRC_nuclei/ --log_dir ./logs_groupDRO_scrc_0
-python examples/run_expt.py --dataset scrc --algorithm IRM --root_dir /home/histopath/Data/SCRC_nuclei/ --log_dir ./logs_IRM_scrc_0
-python examples/run_expt.py --dataset scrc --algorithm ERM --root_dir /home/histopath/Data/SCRC_nuclei/ --log_dir ./logs_ERM_scrc_0
+python examples/run_expt.py --dataset scrc --algorithm deepCORAL --root_dir /home/histopath/Data/SCRC_nuclei/ --split_scheme 012 --log_dir ./logs_deepCORAL_scrc_012_1
+python examples/run_expt.py --dataset scrc --algorithm groupDRO --root_dir /home/histopath/Data/SCRC_nuclei/ --split_scheme 012 --log_dir ./logs_groupDRO_scrc_012_1
+python examples/run_expt.py --dataset scrc --algorithm IRM --root_dir /home/histopath/Data/SCRC_nuclei/ --split_scheme 012 --log_dir ./logs_IRM_scrc_012_1
+python examples/run_expt.py --dataset scrc --algorithm ERM --root_dir /home/histopath/Data/SCRC_nuclei/ --split_scheme 012 --log_dir ./logs_ERM_scrc_012_1
 
-python examples/run_expt.py --dataset scrc --algorithm deepCORAL --root_dir /home/histopath/Data/SCRC_nuclei/ --log_dir ./logs_deepCORAL_scrc_1
-python examples/run_expt.py --dataset scrc --algorithm groupDRO --root_dir /home/histopath/Data/SCRC_nuclei/ --log_dir ./logs_groupDRO_scrc_1
-python examples/run_expt.py --dataset scrc --algorithm IRM --root_dir /home/histopath/Data/SCRC_nuclei/ --log_dir ./logs_IRM_scrc_1
-python examples/run_expt.py --dataset scrc --algorithm ERM --root_dir /home/histopath/Data/SCRC_nuclei/ --log_dir ./logs_ERM_scrc_1
+python examples/run_expt.py --dataset scrc --algorithm deepCORAL --root_dir /home/histopath/Data/SCRC_nuclei/ --split_scheme 012 --log_dir ./logs_deepCORAL_scrc_012_2
+python examples/run_expt.py --dataset scrc --algorithm groupDRO --root_dir /home/histopath/Data/SCRC_nuclei/ --split_scheme 012  --log_dir ./logs_groupDRO_scrc_012_2
+python examples/run_expt.py --dataset scrc --algorithm IRM --root_dir /home/histopath/Data/SCRC_nuclei/ --split_scheme 012 --log_dir ./logs_IRM_scrc_012_2
+python examples/run_expt.py --dataset scrc --algorithm ERM --root_dir /home/histopath/Data/SCRC_nuclei/ --split_scheme 012 --log_dir ./logs_ERM_scrc_012_2
 
-python examples/run_expt.py --dataset scrc --algorithm deepCORAL --root_dir /home/histopath/Data/SCRC_nuclei/ --log_dir ./logs_deepCORAL_scrc_2
-python examples/run_expt.py --dataset scrc --algorithm groupDRO --root_dir /home/histopath/Data/SCRC_nuclei/ --log_dir ./logs_groupDRO_scrc_2
-python examples/run_expt.py --dataset scrc --algorithm IRM --root_dir /home/histopath/Data/SCRC_nuclei/ --log_dir ./logs_IRM_scrc_2
-python examples/run_expt.py --dataset scrc --algorithm ERM --root_dir /home/histopath/Data/SCRC_nuclei/ --log_dir ./logs_ERM_scrc_2
+python examples/run_expt.py --dataset scrc --algorithm deepCORAL --root_dir /home/histopath/Data/SCRC_nuclei/ --split_scheme 012 --log_dir ./logs_deepCORAL_scrc_012_3
+python examples/run_expt.py --dataset scrc --algorithm groupDRO --root_dir /home/histopath/Data/SCRC_nuclei/ --split_scheme 012 --log_dir ./logs_groupDRO_scrc_012_3
+python examples/run_expt.py --dataset scrc --algorithm IRM --root_dir /home/histopath/Data/SCRC_nuclei/ --split_scheme 012 --log_dir ./logs_IRM_scrc_012_3
+python examples/run_expt.py --dataset scrc --algorithm ERM --root_dir /home/histopath/Data/SCRC_nuclei/ --split_scheme 012 --log_dir ./logs_ERM_scrc_012_3
 
-python examples/run_expt.py --dataset scrc --algorithm deepCORAL --root_dir /home/histopath/Data/SCRC_nuclei/ --log_dir ./logs_deepCORAL_scrc_3
-python examples/run_expt.py --dataset scrc --algorithm groupDRO --root_dir /home/histopath/Data/SCRC_nuclei/ --log_dir ./logs_groupDRO_scrc_3
-python examples/run_expt.py --dataset scrc --algorithm IRM --root_dir /home/histopath/Data/SCRC_nuclei/ --log_dir ./logs_IRM_scrc_3
-python examples/run_expt.py --dataset scrc --algorithm ERM --root_dir /home/histopath/Data/SCRC_nuclei/ --log_dir ./logs_ERM_scrc_3
+python examples/run_expt.py --dataset scrc --algorithm deepCORAL --root_dir /home/histopath/Data/SCRC_nuclei/ --split_scheme 201 --log_dir ./logs_deepCORAL_scrc_201_0 
+python examples/run_expt.py --dataset scrc --algorithm groupDRO --root_dir /home/histopath/Data/SCRC_nuclei/ --split_scheme 201 --log_dir ./logs_groupDRO_scrc_201_0
+python examples/run_expt.py --dataset scrc --algorithm IRM --root_dir /home/histopath/Data/SCRC_nuclei/ --split_scheme 201 --log_dir ./logs_IRM_scrc_201_0
+python examples/run_expt.py --dataset scrc --algorithm ERM --root_dir /home/histopath/Data/SCRC_nuclei/ --split_scheme 201 --log_dir ./logs_ERM_scrc_201_0
+
+python examples/run_expt.py --dataset scrc --algorithm deepCORAL --root_dir /home/histopath/Data/SCRC_nuclei/ --split_scheme 201 --log_dir ./logs_deepCORAL_scrc_201_1
+python examples/run_expt.py --dataset scrc --algorithm groupDRO --root_dir /home/histopath/Data/SCRC_nuclei/ --split_scheme 201 --log_dir ./logs_groupDRO_scrc_201_1
+python examples/run_expt.py --dataset scrc --algorithm IRM --root_dir /home/histopath/Data/SCRC_nuclei/ --split_scheme 201 --log_dir ./logs_IRM_scrc_201_1
+python examples/run_expt.py --dataset scrc --algorithm ERM --root_dir /home/histopath/Data/SCRC_nuclei/ --split_scheme 201 --log_dir ./logs_ERM_scrc_201_1
+
+python examples/run_expt.py --dataset scrc --algorithm deepCORAL --root_dir /home/histopath/Data/SCRC_nuclei/ --split_scheme 201 --log_dir ./logs_deepCORAL_scrc_201_2
+python examples/run_expt.py --dataset scrc --algorithm groupDRO --root_dir /home/histopath/Data/SCRC_nuclei/ --split_scheme 201  --log_dir ./logs_groupDRO_scrc_201_2
+python examples/run_expt.py --dataset scrc --algorithm IRM --root_dir /home/histopath/Data/SCRC_nuclei/ --split_scheme 201 --log_dir ./logs_IRM_scrc_201_2
+python examples/run_expt.py --dataset scrc --algorithm ERM --root_dir /home/histopath/Data/SCRC_nuclei/ --split_scheme 201 --log_dir ./logs_ERM_scrc_201_2
+
+python examples/run_expt.py --dataset scrc --algorithm deepCORAL --root_dir /home/histopath/Data/SCRC_nuclei/ --split_scheme 201 --log_dir ./logs_deepCORAL_scrc_201_3
+python examples/run_expt.py --dataset scrc --algorithm groupDRO --root_dir /home/histopath/Data/SCRC_nuclei/ --split_scheme 201 --log_dir ./logs_groupDRO_scrc_201_3
+python examples/run_expt.py --dataset scrc --algorithm IRM --root_dir /home/histopath/Data/SCRC_nuclei/ --split_scheme 201 --log_dir ./logs_IRM_scrc_201_3
+python examples/run_expt.py --dataset scrc --algorithm ERM --root_dir /home/histopath/Data/SCRC_nuclei/ --split_scheme 201 --log_dir ./logs_ERM_scrc_201_3
+
+python examples/run_expt.py --dataset scrc --algorithm deepCORAL --root_dir /home/histopath/Data/SCRC_nuclei/ --split_scheme 120 --log_dir ./logs_deepCORAL_scrc_120_0 
+python examples/run_expt.py --dataset scrc --algorithm groupDRO --root_dir /home/histopath/Data/SCRC_nuclei/ --split_scheme 120 --log_dir ./logs_groupDRO_scrc_120_0
+python examples/run_expt.py --dataset scrc --algorithm IRM --root_dir /home/histopath/Data/SCRC_nuclei/ --split_scheme 120 --log_dir ./logs_IRM_scrc_120_0
+python examples/run_expt.py --dataset scrc --algorithm ERM --root_dir /home/histopath/Data/SCRC_nuclei/ --split_scheme 120 --log_dir ./logs_ERM_scrc_120_0
+
+python examples/run_expt.py --dataset scrc --algorithm deepCORAL --root_dir /home/histopath/Data/SCRC_nuclei/ --split_scheme 120 --log_dir ./logs_deepCORAL_scrc_120_1
+python examples/run_expt.py --dataset scrc --algorithm groupDRO --root_dir /home/histopath/Data/SCRC_nuclei/ --split_scheme 120 --log_dir ./logs_groupDRO_scrc_120_1
+python examples/run_expt.py --dataset scrc --algorithm IRM --root_dir /home/histopath/Data/SCRC_nuclei/ --split_scheme 120 --log_dir ./logs_IRM_scrc_120_1
+python examples/run_expt.py --dataset scrc --algorithm ERM --root_dir /home/histopath/Data/SCRC_nuclei/ --split_scheme 120 --log_dir ./logs_ERM_scrc_120_1
+
+python examples/run_expt.py --dataset scrc --algorithm deepCORAL --root_dir /home/histopath/Data/SCRC_nuclei/ --split_scheme 120 --log_dir ./logs_deepCORAL_scrc_120_2
+python examples/run_expt.py --dataset scrc --algorithm groupDRO --root_dir /home/histopath/Data/SCRC_nuclei/ --split_scheme 120  --log_dir ./logs_groupDRO_scrc_120_2
+python examples/run_expt.py --dataset scrc --algorithm IRM --root_dir /home/histopath/Data/SCRC_nuclei/ --split_scheme 120 --log_dir ./logs_IRM_scrc_120_2
+python examples/run_expt.py --dataset scrc --algorithm ERM --root_dir /home/histopath/Data/SCRC_nuclei/ --split_scheme 120 --log_dir ./logs_ERM_scrc_120_2
+
+python examples/run_expt.py --dataset scrc --algorithm deepCORAL --root_dir /home/histopath/Data/SCRC_nuclei/ --split_scheme 120 --log_dir ./logs_deepCORAL_scrc_120_3
+python examples/run_expt.py --dataset scrc --algorithm groupDRO --root_dir /home/histopath/Data/SCRC_nuclei/ --split_scheme 120 --log_dir ./logs_groupDRO_scrc_120_3
+python examples/run_expt.py --dataset scrc --algorithm IRM --root_dir /home/histopath/Data/SCRC_nuclei/ --split_scheme 120 --log_dir ./logs_IRM_scrc_120_3
+python examples/run_expt.py --dataset scrc --algorithm ERM --root_dir /home/histopath/Data/SCRC_nuclei/ --split_scheme 120 --log_dir ./logs_ERM_scrc_120_3
