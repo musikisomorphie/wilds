400882135d2c391484f75d359e2a3e625be87d3e
musikisomorphie <wujiqing9@gmail.com>
2021-09-16 17:00:33 +0200

    minor chnage

--- run_rxrx1.sh
+++ run_rxrx1.sh
@@ -1,21 +1,21 @@
 #! /bin/bash
 
+python examples/run_expt.py --dataset rxrx1 --algorithm ERM --root_dir /home/histopath/Data/ --log_dir ./logs_ERM_rxrx1_0
 python examples/run_expt.py --dataset rxrx1 --algorithm deepCORAL --root_dir /home/histopath/Data/ --log_dir ./logs_deepCORAL_rxrx1_0
 python examples/run_expt.py --dataset rxrx1 --algorithm groupDRO --root_dir /home/histopath/Data/ --log_dir ./logs_groupDRO_rxrx1_0
 python examples/run_expt.py --dataset rxrx1 --algorithm IRM --root_dir /home/histopath/Data/ --log_dir ./logs_IRM_rxrx1_0
-python examples/run_expt.py --dataset rxrx1 --algorithm ERM --root_dir /home/histopath/Data/ --log_dir ./logs_ERM_rxrx1_0
 
+python examples/run_expt.py --dataset rxrx1 --algorithm ERM --root_dir /home/histopath/Data/ --log_dir ./logs_ERM_rxrx1_1
 python examples/run_expt.py --dataset rxrx1 --algorithm deepCORAL --root_dir /home/histopath/Data/ --log_dir ./logs_deepCORAL_rxrx1_1
 python examples/run_expt.py --dataset rxrx1 --algorithm groupDRO --root_dir /home/histopath/Data/ --log_dir ./logs_groupDRO_rxrx1_1
 python examples/run_expt.py --dataset rxrx1 --algorithm IRM --root_dir /home/histopath/Data/ --log_dir ./logs_IRM_rxrx1_1
-python examples/run_expt.py --dataset rxrx1 --algorithm ERM --root_dir /home/histopath/Data/ --log_dir ./logs_ERM_rxrx1_1
 
+python examples/run_expt.py --dataset rxrx1 --algorithm ERM --root_dir /home/histopath/Data/ --log_dir ./logs_ERM_rxrx1_2
 python examples/run_expt.py --dataset rxrx1 --algorithm deepCORAL --root_dir /home/histopath/Data/ --log_dir ./logs_deepCORAL_rxrx1_2
 python examples/run_expt.py --dataset rxrx1 --algorithm groupDRO --root_dir /home/histopath/Data/ --log_dir ./logs_groupDRO_rxrx1_2
 python examples/run_expt.py --dataset rxrx1 --algorithm IRM --root_dir /home/histopath/Data/ --log_dir ./logs_IRM_rxrx1_2
-python examples/run_expt.py --dataset rxrx1 --algorithm ERM --root_dir /home/histopath/Data/ --log_dir ./logs_ERM_rxrx1_2
 
+python examples/run_expt.py --dataset rxrx1 --algorithm ERM --root_dir /home/histopath/Data/ --log_dir ./logs_ERM_rxrx1_3
 python examples/run_expt.py --dataset rxrx1 --algorithm deepCORAL --root_dir /home/histopath/Data/ --log_dir ./logs_deepCORAL_rxrx1_3
 python examples/run_expt.py --dataset rxrx1 --algorithm groupDRO --root_dir /home/histopath/Data/ --log_dir ./logs_groupDRO_rxrx1_3
 python examples/run_expt.py --dataset rxrx1 --algorithm IRM --root_dir /home/histopath/Data/ --log_dir ./logs_IRM_rxrx1_3
-python examples/run_expt.py --dataset rxrx1 --algorithm ERM --root_dir /home/histopath/Data/ --log_dir ./logs_ERM_rxrx1_3
