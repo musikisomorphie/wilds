8267d654f74db050bc653cc843165a18a1611675
musikisomorphie <wujiqing9@gmail.com>
2021-11-05 16:37:43 +0100

    add eff and vit

--- run_rxrx1_eff_des.sh
+++ run_rxrx1_eff_des.sh
@@ -1,24 +1,24 @@
 #! /bin/bash
 
-python examples/run_expt.py --dataset rxrx1 --model efficientnet-b8 --algorithm ERM       --root_dir /root/Data/ --log_dir /root/Experiment/efficientnet-b8/ERM_rxrx1_0
-python examples/run_expt.py --dataset rxrx1 --model efficientnet-b8 --algorithm deepCORAL --root_dir /root/Data/ --log_dir /root/Experiment/efficientnet-b8/deepCORAL_rxrx1_0
-python examples/run_expt.py --dataset rxrx1 --model efficientnet-b8 --algorithm groupDRO  --root_dir /root/Data/ --log_dir /root/Experiment/efficientnet-b8/groupDRO_rxrx1_0
-python examples/run_expt.py --dataset rxrx1 --model efficientnet-b8 --algorithm IRM       --root_dir /root/Data/ --log_dir /root/Experiment/efficientnet-b8/IRM_rxrx1_0
+python examples/run_expt.py --dataset rxrx1 --model efficientnet-b0 --algorithm ERM       --root_dir /root/Data/ --log_dir /root/Experiment/efficientnet-b0/ERM_rxrx1_0
+python examples/run_expt.py --dataset rxrx1 --model efficientnet-b0 --algorithm deepCORAL --root_dir /root/Data/ --log_dir /root/Experiment/efficientnet-b0/deepCORAL_rxrx1_0
+python examples/run_expt.py --dataset rxrx1 --model efficientnet-b0 --algorithm groupDRO  --root_dir /root/Data/ --log_dir /root/Experiment/efficientnet-b0/groupDRO_rxrx1_0
+python examples/run_expt.py --dataset rxrx1 --model efficientnet-b0 --algorithm IRM       --root_dir /root/Data/ --log_dir /root/Experiment/efficientnet-b0/IRM_rxrx1_0
 
-python examples/run_expt.py --dataset rxrx1 --model efficientnet-b8 --algorithm ERM       --root_dir /root/Data/ --log_dir /root/Experiment/efficientnet-b8/ERM_rxrx1_1
-python examples/run_expt.py --dataset rxrx1 --model efficientnet-b8 --algorithm deepCORAL --root_dir /root/Data/ --log_dir /root/Experiment/efficientnet-b8/deepCORAL_rxrx1_1
-python examples/run_expt.py --dataset rxrx1 --model efficientnet-b8 --algorithm groupDRO  --root_dir /root/Data/ --log_dir /root/Experiment/efficientnet-b8/groupDRO_rxrx1_1
-python examples/run_expt.py --dataset rxrx1 --model efficientnet-b8 --algorithm IRM       --root_dir /root/Data/ --log_dir /root/Experiment/efficientnet-b8/IRM_rxrx1_1
+python examples/run_expt.py --dataset rxrx1 --model efficientnet-b0 --algorithm ERM       --root_dir /root/Data/ --log_dir /root/Experiment/efficientnet-b0/ERM_rxrx1_1
+python examples/run_expt.py --dataset rxrx1 --model efficientnet-b0 --algorithm deepCORAL --root_dir /root/Data/ --log_dir /root/Experiment/efficientnet-b0/deepCORAL_rxrx1_1
+python examples/run_expt.py --dataset rxrx1 --model efficientnet-b0 --algorithm groupDRO  --root_dir /root/Data/ --log_dir /root/Experiment/efficientnet-b0/groupDRO_rxrx1_1
+python examples/run_expt.py --dataset rxrx1 --model efficientnet-b0 --algorithm IRM       --root_dir /root/Data/ --log_dir /root/Experiment/efficientnet-b0/IRM_rxrx1_1
 
-python examples/run_expt.py --dataset rxrx1 --model efficientnet-b8 --algorithm ERM       --root_dir /root/Data/ --log_dir /root/Experiment/efficientnet-b8/ERM_rxrx1_2
-python examples/run_expt.py --dataset rxrx1 --model efficientnet-b8 --algorithm deepCORAL --root_dir /root/Data/ --log_dir /root/Experiment/efficientnet-b8/deepCORAL_rxrx1_2
-python examples/run_expt.py --dataset rxrx1 --model efficientnet-b8 --algorithm groupDRO  --root_dir /root/Data/ --log_dir /root/Experiment/efficientnet-b8/groupDRO_rxrx1_2
-python examples/run_expt.py --dataset rxrx1 --model efficientnet-b8 --algorithm IRM       --root_dir /root/Data/ --log_dir /root/Experiment/efficientnet-b8/IRM_rxrx1_2
+python examples/run_expt.py --dataset rxrx1 --model efficientnet-b0 --algorithm ERM       --root_dir /root/Data/ --log_dir /root/Experiment/efficientnet-b0/ERM_rxrx1_2
+python examples/run_expt.py --dataset rxrx1 --model efficientnet-b0 --algorithm deepCORAL --root_dir /root/Data/ --log_dir /root/Experiment/efficientnet-b0/deepCORAL_rxrx1_2
+python examples/run_expt.py --dataset rxrx1 --model efficientnet-b0 --algorithm groupDRO  --root_dir /root/Data/ --log_dir /root/Experiment/efficientnet-b0/groupDRO_rxrx1_2
+python examples/run_expt.py --dataset rxrx1 --model efficientnet-b0 --algorithm IRM       --root_dir /root/Data/ --log_dir /root/Experiment/efficientnet-b0/IRM_rxrx1_2
 
-python examples/run_expt.py --dataset rxrx1 --model efficientnet-b8 --algorithm ERM       --root_dir /root/Data/ --log_dir /root/Experiment/efficientnet-b8/ERM_rxrx1_3
-python examples/run_expt.py --dataset rxrx1 --model efficientnet-b8 --algorithm deepCORAL --root_dir /root/Data/ --log_dir /root/Experiment/efficientnet-b8/deepCORAL_rxrx1_3
-python examples/run_expt.py --dataset rxrx1 --model efficientnet-b8 --algorithm groupDRO  --root_dir /root/Data/ --log_dir /root/Experiment/efficientnet-b8/groupDRO_rxrx1_3
-python examples/run_expt.py --dataset rxrx1 --model efficientnet-b8 --algorithm IRM       --root_dir /root/Data/ --log_dir /root/Experiment/efficientnet-b8/IRM_rxrx1_3
+python examples/run_expt.py --dataset rxrx1 --model efficientnet-b0 --algorithm ERM       --root_dir /root/Data/ --log_dir /root/Experiment/efficientnet-b0/ERM_rxrx1_3
+python examples/run_expt.py --dataset rxrx1 --model efficientnet-b0 --algorithm deepCORAL --root_dir /root/Data/ --log_dir /root/Experiment/efficientnet-b0/deepCORAL_rxrx1_3
+python examples/run_expt.py --dataset rxrx1 --model efficientnet-b0 --algorithm groupDRO  --root_dir /root/Data/ --log_dir /root/Experiment/efficientnet-b0/groupDRO_rxrx1_3
+python examples/run_expt.py --dataset rxrx1 --model efficientnet-b0 --algorithm IRM       --root_dir /root/Data/ --log_dir /root/Experiment/efficientnet-b0/IRM_rxrx1_3
 
 
 python examples/run_expt.py --dataset rxrx1 --model densenet121 --algorithm ERM       --root_dir /root/Data/ --log_dir /root/Experiment/densenet121/ERM_rxrx1_0
