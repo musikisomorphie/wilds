620e59377a21c94fe46c5cd517b17b526fad8683
musikisomorphie <wujiqing9@gmail.com>
2021-11-05 16:45:50 +0100

    add eff and vit

--- run_rxrx1_vit_res.sh
+++ run_rxrx1_vit_res.sh
@@ -1,24 +1,24 @@
 #! /bin/bash
 
-python examples/run_expt.py --dataset rxrx1 --model vit --batch_size 144 --algorithm deepCORAL --root_dir /root/Data/ --log_dir /root/Experiment/vit/deepCORAL_rxrx1_0
-python examples/run_expt.py --dataset rxrx1 --model vit --batch_size 144 --algorithm ERM       --root_dir /root/Data/ --log_dir /root/Experiment/vit/ERM_rxrx1_0
-python examples/run_expt.py --dataset rxrx1 --model vit --batch_size 144 --algorithm groupDRO  --root_dir /root/Data/ --log_dir /root/Experiment/vit/groupDRO_rxrx1_0
-python examples/run_expt.py --dataset rxrx1 --model vit --batch_size 144 --algorithm IRM       --root_dir /root/Data/ --log_dir /root/Experiment/vit/IRM_rxrx1_0
+python examples/run_expt.py --dataset rxrx1 --model vit --algorithm deepCORAL --root_dir /root/Data/ --log_dir /root/Experiment/vit/deepCORAL_rxrx1_0
+python examples/run_expt.py --dataset rxrx1 --model vit --algorithm ERM       --root_dir /root/Data/ --log_dir /root/Experiment/vit/ERM_rxrx1_0
+python examples/run_expt.py --dataset rxrx1 --model vit --algorithm groupDRO  --root_dir /root/Data/ --log_dir /root/Experiment/vit/groupDRO_rxrx1_0
+python examples/run_expt.py --dataset rxrx1 --model vit --algorithm IRM       --root_dir /root/Data/ --log_dir /root/Experiment/vit/IRM_rxrx1_0
 
-python examples/run_expt.py --dataset rxrx1 --model vit --batch_size 144 --algorithm deepCORAL --root_dir /root/Data/ --log_dir /root/Experiment/vit/deepCORAL_rxrx1_1
-python examples/run_expt.py --dataset rxrx1 --model vit --batch_size 144 --algorithm ERM       --root_dir /root/Data/ --log_dir /root/Experiment/vit/ERM_rxrx1_1
-python examples/run_expt.py --dataset rxrx1 --model vit --batch_size 144 --algorithm groupDRO  --root_dir /root/Data/ --log_dir /root/Experiment/vit/groupDRO_rxrx1_1
-python examples/run_expt.py --dataset rxrx1 --model vit --batch_size 144 --algorithm IRM       --root_dir /root/Data/ --log_dir /root/Experiment/vit/IRM_rxrx1_1
+python examples/run_expt.py --dataset rxrx1 --model vit --algorithm deepCORAL --root_dir /root/Data/ --log_dir /root/Experiment/vit/deepCORAL_rxrx1_1
+python examples/run_expt.py --dataset rxrx1 --model vit --algorithm ERM       --root_dir /root/Data/ --log_dir /root/Experiment/vit/ERM_rxrx1_1
+python examples/run_expt.py --dataset rxrx1 --model vit --algorithm groupDRO  --root_dir /root/Data/ --log_dir /root/Experiment/vit/groupDRO_rxrx1_1
+python examples/run_expt.py --dataset rxrx1 --model vit --algorithm IRM       --root_dir /root/Data/ --log_dir /root/Experiment/vit/IRM_rxrx1_1
 
-python examples/run_expt.py --dataset rxrx1 --model vit --batch_size 144 --algorithm deepCORAL --root_dir /root/Data/ --log_dir /root/Experiment/vit/deepCORAL_rxrx1_2
-python examples/run_expt.py --dataset rxrx1 --model vit --batch_size 144 --algorithm ERM       --root_dir /root/Data/ --log_dir /root/Experiment/vit/ERM_rxrx1_2
-python examples/run_expt.py --dataset rxrx1 --model vit --batch_size 144 --algorithm groupDRO  --root_dir /root/Data/ --log_dir /root/Experiment/vit/groupDRO_rxrx1_2
-python examples/run_expt.py --dataset rxrx1 --model vit --batch_size 144 --algorithm IRM       --root_dir /root/Data/ --log_dir /root/Experiment/vit/IRM_rxrx1_2
+python examples/run_expt.py --dataset rxrx1 --model vit --algorithm deepCORAL --root_dir /root/Data/ --log_dir /root/Experiment/vit/deepCORAL_rxrx1_2
+python examples/run_expt.py --dataset rxrx1 --model vit --algorithm ERM       --root_dir /root/Data/ --log_dir /root/Experiment/vit/ERM_rxrx1_2
+python examples/run_expt.py --dataset rxrx1 --model vit --algorithm groupDRO  --root_dir /root/Data/ --log_dir /root/Experiment/vit/groupDRO_rxrx1_2
+python examples/run_expt.py --dataset rxrx1 --model vit --algorithm IRM       --root_dir /root/Data/ --log_dir /root/Experiment/vit/IRM_rxrx1_2
 
-python examples/run_expt.py --dataset rxrx1 --model vit --batch_size 144 --algorithm deepCORAL --root_dir /root/Data/ --log_dir /root/Experiment/vit/deepCORAL_rxrx1_3
-python examples/run_expt.py --dataset rxrx1 --model vit --batch_size 144 --algorithm ERM       --root_dir /root/Data/ --log_dir /root/Experiment/vit/ERM_rxrx1_3
-python examples/run_expt.py --dataset rxrx1 --model vit --batch_size 144 --algorithm groupDRO  --root_dir /root/Data/ --log_dir /root/Experiment/vit/groupDRO_rxrx1_3
-python examples/run_expt.py --dataset rxrx1 --model vit --batch_size 144 --algorithm IRM       --root_dir /root/Data/ --log_dir /root/Experiment/vit/IRM_rxrx1_3
+python examples/run_expt.py --dataset rxrx1 --model vit --algorithm deepCORAL --root_dir /root/Data/ --log_dir /root/Experiment/vit/deepCORAL_rxrx1_3
+python examples/run_expt.py --dataset rxrx1 --model vit --algorithm ERM       --root_dir /root/Data/ --log_dir /root/Experiment/vit/ERM_rxrx1_3
+python examples/run_expt.py --dataset rxrx1 --model vit --algorithm groupDRO  --root_dir /root/Data/ --log_dir /root/Experiment/vit/groupDRO_rxrx1_3
+python examples/run_expt.py --dataset rxrx1 --model vit --algorithm IRM       --root_dir /root/Data/ --log_dir /root/Experiment/vit/IRM_rxrx1_3
 
 
 python examples/run_expt.py --dataset rxrx1 --model resnet50 --algorithm ERM       --root_dir /root/Data/ --log_dir /root/Experiment/resnet50/ERM_rxrx1_0
--- run_scrc_vit.sh
+++ run_scrc_vit.sh
@@ -1,61 +1,61 @@
 #! /bin/bash
 
-python examples/run_expt.py --dataset scrc --model vit --batch_size 128  --algorithm deepCORAL --root_dir /root/Data/ --split_scheme 012 --log_dir /root/Experiment/vit/deepCORAL_scrc_012_0
-python examples/run_expt.py --dataset scrc --model vit --batch_size 128  --algorithm ERM       --root_dir /root/Data/ --split_scheme 012 --log_dir /root/Experiment/vit/ERM_scrc_012_0 
-python examples/run_expt.py --dataset scrc --model vit --batch_size 128  --algorithm groupDRO  --root_dir /root/Data/ --split_scheme 012 --log_dir /root/Experiment/vit/groupDRO_scrc_012_0
-python examples/run_expt.py --dataset scrc --model vit --batch_size 128  --algorithm IRM       --root_dir /root/Data/ --split_scheme 012 --log_dir /root/Experiment/vit/IRM_scrc_012_0
+python examples/run_expt.py --dataset scrc --model vit  --algorithm deepCORAL --root_dir /root/Data/ --split_scheme 012 --log_dir /root/Experiment/vit/deepCORAL_scrc_012_0
+python examples/run_expt.py --dataset scrc --model vit  --algorithm ERM       --root_dir /root/Data/ --split_scheme 012 --log_dir /root/Experiment/vit/ERM_scrc_012_0 
+python examples/run_expt.py --dataset scrc --model vit  --algorithm groupDRO  --root_dir /root/Data/ --split_scheme 012 --log_dir /root/Experiment/vit/groupDRO_scrc_012_0
+python examples/run_expt.py --dataset scrc --model vit  --algorithm IRM       --root_dir /root/Data/ --split_scheme 012 --log_dir /root/Experiment/vit/IRM_scrc_012_0
 
-python examples/run_expt.py --dataset scrc --model vit --batch_size 128  --algorithm deepCORAL --root_dir /root/Data/ --split_scheme 012 --log_dir /root/Experiment/vit/deepCORAL_scrc_012_1
-python examples/run_expt.py --dataset scrc --model vit --batch_size 128  --algorithm ERM       --root_dir /root/Data/ --split_scheme 012 --log_dir /root/Experiment/vit/ERM_scrc_012_1
-python examples/run_expt.py --dataset scrc --model vit --batch_size 128  --algorithm groupDRO  --root_dir /root/Data/ --split_scheme 012 --log_dir /root/Experiment/vit/groupDRO_scrc_012_1
-python examples/run_expt.py --dataset scrc --model vit --batch_size 128  --algorithm IRM       --root_dir /root/Data/ --split_scheme 012 --log_dir /root/Experiment/vit/IRM_scrc_012_1
+python examples/run_expt.py --dataset scrc --model vit  --algorithm deepCORAL --root_dir /root/Data/ --split_scheme 012 --log_dir /root/Experiment/vit/deepCORAL_scrc_012_1
+python examples/run_expt.py --dataset scrc --model vit  --algorithm ERM       --root_dir /root/Data/ --split_scheme 012 --log_dir /root/Experiment/vit/ERM_scrc_012_1
+python examples/run_expt.py --dataset scrc --model vit  --algorithm groupDRO  --root_dir /root/Data/ --split_scheme 012 --log_dir /root/Experiment/vit/groupDRO_scrc_012_1
+python examples/run_expt.py --dataset scrc --model vit  --algorithm IRM       --root_dir /root/Data/ --split_scheme 012 --log_dir /root/Experiment/vit/IRM_scrc_012_1
 
-python examples/run_expt.py --dataset scrc --model vit --batch_size 128  --algorithm deepCORAL --root_dir /root/Data/ --split_scheme 012 --log_dir /root/Experiment/vit/deepCORAL_scrc_012_2
-python examples/run_expt.py --dataset scrc --model vit --batch_size 128  --algorithm ERM       --root_dir /root/Data/ --split_scheme 012 --log_dir /root/Experiment/vit/ERM_scrc_012_2
-python examples/run_expt.py --dataset scrc --model vit --batch_size 128  --algorithm groupDRO  --root_dir /root/Data/ --split_scheme 012 --log_dir /root/Experiment/vit/groupDRO_scrc_012_2
-python examples/run_expt.py --dataset scrc --model vit --batch_size 128  --algorithm IRM       --root_dir /root/Data/ --split_scheme 012 --log_dir /root/Experiment/vit/IRM_scrc_012_2
+python examples/run_expt.py --dataset scrc --model vit  --algorithm deepCORAL --root_dir /root/Data/ --split_scheme 012 --log_dir /root/Experiment/vit/deepCORAL_scrc_012_2
+python examples/run_expt.py --dataset scrc --model vit  --algorithm ERM       --root_dir /root/Data/ --split_scheme 012 --log_dir /root/Experiment/vit/ERM_scrc_012_2
+python examples/run_expt.py --dataset scrc --model vit  --algorithm groupDRO  --root_dir /root/Data/ --split_scheme 012 --log_dir /root/Experiment/vit/groupDRO_scrc_012_2
+python examples/run_expt.py --dataset scrc --model vit  --algorithm IRM       --root_dir /root/Data/ --split_scheme 012 --log_dir /root/Experiment/vit/IRM_scrc_012_2
 
-python examples/run_expt.py --dataset scrc --model vit --batch_size 128  --algorithm deepCORAL --root_dir /root/Data/ --split_scheme 012 --log_dir /root/Experiment/vit/deepCORAL_scrc_012_3
-python examples/run_expt.py --dataset scrc --model vit --batch_size 128  --algorithm ERM       --root_dir /root/Data/ --split_scheme 012 --log_dir /root/Experiment/vit/ERM_scrc_012_3
-python examples/run_expt.py --dataset scrc --model vit --batch_size 128  --algorithm groupDRO  --root_dir /root/Data/ --split_scheme 012 --log_dir /root/Experiment/vit/groupDRO_scrc_012_3
-python examples/run_expt.py --dataset scrc --model vit --batch_size 128  --algorithm IRM       --root_dir /root/Data/ --split_scheme 012 --log_dir /root/Experiment/vit/IRM_scrc_012_3
+python examples/run_expt.py --dataset scrc --model vit  --algorithm deepCORAL --root_dir /root/Data/ --split_scheme 012 --log_dir /root/Experiment/vit/deepCORAL_scrc_012_3
+python examples/run_expt.py --dataset scrc --model vit  --algorithm ERM       --root_dir /root/Data/ --split_scheme 012 --log_dir /root/Experiment/vit/ERM_scrc_012_3
+python examples/run_expt.py --dataset scrc --model vit  --algorithm groupDRO  --root_dir /root/Data/ --split_scheme 012 --log_dir /root/Experiment/vit/groupDRO_scrc_012_3
+python examples/run_expt.py --dataset scrc --model vit  --algorithm IRM       --root_dir /root/Data/ --split_scheme 012 --log_dir /root/Experiment/vit/IRM_scrc_012_3
 
-python examples/run_expt.py --dataset scrc --model vit --batch_size 128  --algorithm deepCORAL --root_dir /root/Data/ --split_scheme 201 --log_dir /root/Experiment/vit/deepCORAL_scrc_201_0 
-python examples/run_expt.py --dataset scrc --model vit --batch_size 128  --algorithm ERM       --root_dir /root/Data/ --split_scheme 201 --log_dir /root/Experiment/vit/ERM_scrc_201_0
-python examples/run_expt.py --dataset scrc --model vit --batch_size 128  --algorithm groupDRO  --root_dir /root/Data/ --split_scheme 201 --log_dir /root/Experiment/vit/groupDRO_scrc_201_0
-python examples/run_expt.py --dataset scrc --model vit --batch_size 128  --algorithm IRM       --root_dir /root/Data/ --split_scheme 201 --log_dir /root/Experiment/vit/IRM_scrc_201_0
+python examples/run_expt.py --dataset scrc --model vit  --algorithm deepCORAL --root_dir /root/Data/ --split_scheme 201 --log_dir /root/Experiment/vit/deepCORAL_scrc_201_0 
+python examples/run_expt.py --dataset scrc --model vit  --algorithm ERM       --root_dir /root/Data/ --split_scheme 201 --log_dir /root/Experiment/vit/ERM_scrc_201_0
+python examples/run_expt.py --dataset scrc --model vit  --algorithm groupDRO  --root_dir /root/Data/ --split_scheme 201 --log_dir /root/Experiment/vit/groupDRO_scrc_201_0
+python examples/run_expt.py --dataset scrc --model vit  --algorithm IRM       --root_dir /root/Data/ --split_scheme 201 --log_dir /root/Experiment/vit/IRM_scrc_201_0
 
-python examples/run_expt.py --dataset scrc --model vit --batch_size 128  --algorithm deepCORAL --root_dir /root/Data/ --split_scheme 201 --log_dir /root/Experiment/vit/deepCORAL_scrc_201_1
-python examples/run_expt.py --dataset scrc --model vit --batch_size 128  --algorithm ERM       --root_dir /root/Data/ --split_scheme 201 --log_dir /root/Experiment/vit/ERM_scrc_201_1
-python examples/run_expt.py --dataset scrc --model vit --batch_size 128  --algorithm groupDRO  --root_dir /root/Data/ --split_scheme 201 --log_dir /root/Experiment/vit/groupDRO_scrc_201_1
-python examples/run_expt.py --dataset scrc --model vit --batch_size 128  --algorithm IRM       --root_dir /root/Data/ --split_scheme 201 --log_dir /root/Experiment/vit/IRM_scrc_201_1
+python examples/run_expt.py --dataset scrc --model vit  --algorithm deepCORAL --root_dir /root/Data/ --split_scheme 201 --log_dir /root/Experiment/vit/deepCORAL_scrc_201_1
+python examples/run_expt.py --dataset scrc --model vit  --algorithm ERM       --root_dir /root/Data/ --split_scheme 201 --log_dir /root/Experiment/vit/ERM_scrc_201_1
+python examples/run_expt.py --dataset scrc --model vit  --algorithm groupDRO  --root_dir /root/Data/ --split_scheme 201 --log_dir /root/Experiment/vit/groupDRO_scrc_201_1
+python examples/run_expt.py --dataset scrc --model vit  --algorithm IRM       --root_dir /root/Data/ --split_scheme 201 --log_dir /root/Experiment/vit/IRM_scrc_201_1
 
-python examples/run_expt.py --dataset scrc --model vit --batch_size 128  --algorithm deepCORAL --root_dir /root/Data/ --split_scheme 201 --log_dir /root/Experiment/vit/deepCORAL_scrc_201_2
-python examples/run_expt.py --dataset scrc --model vit --batch_size 128  --algorithm ERM       --root_dir /root/Data/ --split_scheme 201 --log_dir /root/Experiment/vit/ERM_scrc_201_2
-python examples/run_expt.py --dataset scrc --model vit --batch_size 128  --algorithm groupDRO  --root_dir /root/Data/ --split_scheme 201 --log_dir /root/Experiment/vit/groupDRO_scrc_201_2
-python examples/run_expt.py --dataset scrc --model vit --batch_size 128  --algorithm IRM       --root_dir /root/Data/ --split_scheme 201 --log_dir /root/Experiment/vit/IRM_scrc_201_2
+python examples/run_expt.py --dataset scrc --model vit  --algorithm deepCORAL --root_dir /root/Data/ --split_scheme 201 --log_dir /root/Experiment/vit/deepCORAL_scrc_201_2
+python examples/run_expt.py --dataset scrc --model vit  --algorithm ERM       --root_dir /root/Data/ --split_scheme 201 --log_dir /root/Experiment/vit/ERM_scrc_201_2
+python examples/run_expt.py --dataset scrc --model vit  --algorithm groupDRO  --root_dir /root/Data/ --split_scheme 201 --log_dir /root/Experiment/vit/groupDRO_scrc_201_2
+python examples/run_expt.py --dataset scrc --model vit  --algorithm IRM       --root_dir /root/Data/ --split_scheme 201 --log_dir /root/Experiment/vit/IRM_scrc_201_2
 
-python examples/run_expt.py --dataset scrc --model vit --batch_size 128  --algorithm deepCORAL --root_dir /root/Data/ --split_scheme 201 --log_dir /root/Experiment/vit/deepCORAL_scrc_201_3
-python examples/run_expt.py --dataset scrc --model vit --batch_size 128  --algorithm ERM       --root_dir /root/Data/ --split_scheme 201 --log_dir /root/Experiment/vit/ERM_scrc_201_3
-python examples/run_expt.py --dataset scrc --model vit --batch_size 128  --algorithm groupDRO  --root_dir /root/Data/ --split_scheme 201 --log_dir /root/Experiment/vit/groupDRO_scrc_201_3
-python examples/run_expt.py --dataset scrc --model vit --batch_size 128  --algorithm IRM       --root_dir /root/Data/ --split_scheme 201 --log_dir /root/Experiment/vit/IRM_scrc_201_3
+python examples/run_expt.py --dataset scrc --model vit  --algorithm deepCORAL --root_dir /root/Data/ --split_scheme 201 --log_dir /root/Experiment/vit/deepCORAL_scrc_201_3
+python examples/run_expt.py --dataset scrc --model vit  --algorithm ERM       --root_dir /root/Data/ --split_scheme 201 --log_dir /root/Experiment/vit/ERM_scrc_201_3
+python examples/run_expt.py --dataset scrc --model vit  --algorithm groupDRO  --root_dir /root/Data/ --split_scheme 201 --log_dir /root/Experiment/vit/groupDRO_scrc_201_3
+python examples/run_expt.py --dataset scrc --model vit  --algorithm IRM       --root_dir /root/Data/ --split_scheme 201 --log_dir /root/Experiment/vit/IRM_scrc_201_3
 
-python examples/run_expt.py --dataset scrc --model vit --batch_size 128  --algorithm deepCORAL --root_dir /root/Data/ --split_scheme 120 --log_dir /root/Experiment/vit/deepCORAL_scrc_120_0 
-python examples/run_expt.py --dataset scrc --model vit --batch_size 128  --algorithm ERM       --root_dir /root/Data/ --split_scheme 120 --log_dir /root/Experiment/vit/ERM_scrc_120_0
-python examples/run_expt.py --dataset scrc --model vit --batch_size 128  --algorithm groupDRO  --root_dir /root/Data/ --split_scheme 120 --log_dir /root/Experiment/vit/groupDRO_scrc_120_0
-python examples/run_expt.py --dataset scrc --model vit --batch_size 128  --algorithm IRM       --root_dir /root/Data/ --split_scheme 120 --log_dir /root/Experiment/vit/IRM_scrc_120_0
+python examples/run_expt.py --dataset scrc --model vit  --algorithm deepCORAL --root_dir /root/Data/ --split_scheme 120 --log_dir /root/Experiment/vit/deepCORAL_scrc_120_0 
+python examples/run_expt.py --dataset scrc --model vit  --algorithm ERM       --root_dir /root/Data/ --split_scheme 120 --log_dir /root/Experiment/vit/ERM_scrc_120_0
+python examples/run_expt.py --dataset scrc --model vit  --algorithm groupDRO  --root_dir /root/Data/ --split_scheme 120 --log_dir /root/Experiment/vit/groupDRO_scrc_120_0
+python examples/run_expt.py --dataset scrc --model vit  --algorithm IRM       --root_dir /root/Data/ --split_scheme 120 --log_dir /root/Experiment/vit/IRM_scrc_120_0
 
-python examples/run_expt.py --dataset scrc --model vit --batch_size 128  --algorithm deepCORAL --root_dir /root/Data/ --split_scheme 120 --log_dir /root/Experiment/vit/deepCORAL_scrc_120_1
-python examples/run_expt.py --dataset scrc --model vit --batch_size 128  --algorithm ERM       --root_dir /root/Data/ --split_scheme 120 --log_dir /root/Experiment/vit/ERM_scrc_120_1
-python examples/run_expt.py --dataset scrc --model vit --batch_size 128  --algorithm groupDRO  --root_dir /root/Data/ --split_scheme 120 --log_dir /root/Experiment/vit/groupDRO_scrc_120_1
-python examples/run_expt.py --dataset scrc --model vit --batch_size 128  --algorithm IRM       --root_dir /root/Data/ --split_scheme 120 --log_dir /root/Experiment/vit/IRM_scrc_120_1
+python examples/run_expt.py --dataset scrc --model vit  --algorithm deepCORAL --root_dir /root/Data/ --split_scheme 120 --log_dir /root/Experiment/vit/deepCORAL_scrc_120_1
+python examples/run_expt.py --dataset scrc --model vit  --algorithm ERM       --root_dir /root/Data/ --split_scheme 120 --log_dir /root/Experiment/vit/ERM_scrc_120_1
+python examples/run_expt.py --dataset scrc --model vit  --algorithm groupDRO  --root_dir /root/Data/ --split_scheme 120 --log_dir /root/Experiment/vit/groupDRO_scrc_120_1
+python examples/run_expt.py --dataset scrc --model vit  --algorithm IRM       --root_dir /root/Data/ --split_scheme 120 --log_dir /root/Experiment/vit/IRM_scrc_120_1
 
-python examples/run_expt.py --dataset scrc --model vit --batch_size 128  --algorithm deepCORAL --root_dir /root/Data/ --split_scheme 120 --log_dir /root/Experiment/vit/deepCORAL_scrc_120_2
-python examples/run_expt.py --dataset scrc --model vit --batch_size 128  --algorithm ERM       --root_dir /root/Data/ --split_scheme 120 --log_dir /root/Experiment/vit/ERM_scrc_120_2
-python examples/run_expt.py --dataset scrc --model vit --batch_size 128  --algorithm groupDRO  --root_dir /root/Data/ --split_scheme 120 --log_dir /root/Experiment/vit/groupDRO_scrc_120_2
-python examples/run_expt.py --dataset scrc --model vit --batch_size 128  --algorithm IRM       --root_dir /root/Data/ --split_scheme 120 --log_dir /root/Experiment/vit/IRM_scrc_120_2
+python examples/run_expt.py --dataset scrc --model vit  --algorithm deepCORAL --root_dir /root/Data/ --split_scheme 120 --log_dir /root/Experiment/vit/deepCORAL_scrc_120_2
+python examples/run_expt.py --dataset scrc --model vit  --algorithm ERM       --root_dir /root/Data/ --split_scheme 120 --log_dir /root/Experiment/vit/ERM_scrc_120_2
+python examples/run_expt.py --dataset scrc --model vit  --algorithm groupDRO  --root_dir /root/Data/ --split_scheme 120 --log_dir /root/Experiment/vit/groupDRO_scrc_120_2
+python examples/run_expt.py --dataset scrc --model vit  --algorithm IRM       --root_dir /root/Data/ --split_scheme 120 --log_dir /root/Experiment/vit/IRM_scrc_120_2
 
-python examples/run_expt.py --dataset scrc --model vit --batch_size 128  --algorithm deepCORAL --root_dir /root/Data/ --split_scheme 120 --log_dir /root/Experiment/vit/deepCORAL_scrc_120_3
-python examples/run_expt.py --dataset scrc --model vit --batch_size 128  --algorithm ERM       --root_dir /root/Data/ --split_scheme 120 --log_dir /root/Experiment/vit/ERM_scrc_120_3
-python examples/run_expt.py --dataset scrc --model vit --batch_size 128  --algorithm groupDRO  --root_dir /root/Data/ --split_scheme 120 --log_dir /root/Experiment/vit/groupDRO_scrc_120_3
-python examples/run_expt.py --dataset scrc --model vit --batch_size 128  --algorithm IRM       --root_dir /root/Data/ --split_scheme 120 --log_dir /root/Experiment/vit/IRM_scrc_120_3
+python examples/run_expt.py --dataset scrc --model vit  --algorithm deepCORAL --root_dir /root/Data/ --split_scheme 120 --log_dir /root/Experiment/vit/deepCORAL_scrc_120_3
+python examples/run_expt.py --dataset scrc --model vit  --algorithm ERM       --root_dir /root/Data/ --split_scheme 120 --log_dir /root/Experiment/vit/ERM_scrc_120_3
+python examples/run_expt.py --dataset scrc --model vit  --algorithm groupDRO  --root_dir /root/Data/ --split_scheme 120 --log_dir /root/Experiment/vit/groupDRO_scrc_120_3
+python examples/run_expt.py --dataset scrc --model vit  --algorithm IRM       --root_dir /root/Data/ --split_scheme 120 --log_dir /root/Experiment/vit/IRM_scrc_120_3
