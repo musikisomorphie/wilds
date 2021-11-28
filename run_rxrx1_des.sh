c209c7e63cadceb25b76a005cf99716e44545e34
musikisomorphie <wujiqing9@gmail.com>
2021-11-28 10:45:48 +0100

    change sh files

--- run_rxrx1_des.sh
+++ run_rxrx1_des.sh
@@ -1,21 +1,21 @@
 #! /bin/bash
 
-python examples/run_expt.py --dataset rxrx1 --model densenet121 --algorithm ERM       --root_dir /root/Data/ --seed 0 --log_dir /root/Experiment/densenet121/ERM_rxrx1_0
-python examples/run_expt.py --dataset rxrx1 --model densenet121 --algorithm deepCORAL --root_dir /root/Data/ --seed 0 --log_dir /root/Experiment/densenet121/deepCORAL_rxrx1_0
-python examples/run_expt.py --dataset rxrx1 --model densenet121 --algorithm groupDRO  --root_dir /root/Data/ --seed 0 --log_dir /root/Experiment/densenet121/groupDRO_rxrx1_0
-python examples/run_expt.py --dataset rxrx1 --model densenet121 --algorithm IRM       --root_dir /root/Data/ --seed 0 --log_dir /root/Experiment/densenet121/IRM_rxrx1_0
+python examples/run_expt.py --dataset rxrx1 --model densenet121 --algorithm ERM       --root_dir /root/Data/ --seed 0 --log_dir /root/Experiment/wilds/densenet121/ERM_rxrx1_0
+python examples/run_expt.py --dataset rxrx1 --model densenet121 --algorithm deepCORAL --root_dir /root/Data/ --seed 0 --log_dir /root/Experiment/wilds/densenet121/deepCORAL_rxrx1_0
+python examples/run_expt.py --dataset rxrx1 --model densenet121 --algorithm groupDRO  --root_dir /root/Data/ --seed 0 --log_dir /root/Experiment/wilds/densenet121/groupDRO_rxrx1_0
+python examples/run_expt.py --dataset rxrx1 --model densenet121 --algorithm IRM       --root_dir /root/Data/ --seed 0 --log_dir /root/Experiment/wilds/densenet121/IRM_rxrx1_0
 
-python examples/run_expt.py --dataset rxrx1 --model densenet121 --algorithm ERM       --root_dir /root/Data/ --seed 32 --log_dir /root/Experiment/densenet121/ERM_rxrx1_1
-python examples/run_expt.py --dataset rxrx1 --model densenet121 --algorithm deepCORAL --root_dir /root/Data/ --seed 32 --log_dir /root/Experiment/densenet121/deepCORAL_rxrx1_1
-python examples/run_expt.py --dataset rxrx1 --model densenet121 --algorithm groupDRO  --root_dir /root/Data/ --seed 32 --log_dir /root/Experiment/densenet121/groupDRO_rxrx1_1
-python examples/run_expt.py --dataset rxrx1 --model densenet121 --algorithm IRM       --root_dir /root/Data/ --seed 32 --log_dir /root/Experiment/densenet121/IRM_rxrx1_1
+python examples/run_expt.py --dataset rxrx1 --model densenet121 --algorithm ERM       --root_dir /root/Data/ --seed 32 --log_dir /root/Experiment/wilds/densenet121/ERM_rxrx1_1
+python examples/run_expt.py --dataset rxrx1 --model densenet121 --algorithm deepCORAL --root_dir /root/Data/ --seed 32 --log_dir /root/Experiment/wilds/densenet121/deepCORAL_rxrx1_1
+python examples/run_expt.py --dataset rxrx1 --model densenet121 --algorithm groupDRO  --root_dir /root/Data/ --seed 32 --log_dir /root/Experiment/wilds/densenet121/groupDRO_rxrx1_1
+python examples/run_expt.py --dataset rxrx1 --model densenet121 --algorithm IRM       --root_dir /root/Data/ --seed 32 --log_dir /root/Experiment/wilds/densenet121/IRM_rxrx1_1
 
-python examples/run_expt.py --dataset rxrx1 --model densenet121 --algorithm ERM       --root_dir /root/Data/ --seed 512 --log_dir /root/Experiment/densenet121/ERM_rxrx1_2
-python examples/run_expt.py --dataset rxrx1 --model densenet121 --algorithm deepCORAL --root_dir /root/Data/ --seed 512 --log_dir /root/Experiment/densenet121/deepCORAL_rxrx1_2
-python examples/run_expt.py --dataset rxrx1 --model densenet121 --algorithm groupDRO  --root_dir /root/Data/ --seed 512 --log_dir /root/Experiment/densenet121/groupDRO_rxrx1_2
-python examples/run_expt.py --dataset rxrx1 --model densenet121 --algorithm IRM       --root_dir /root/Data/ --seed 512 --log_dir /root/Experiment/densenet121/IRM_rxrx1_2
+python examples/run_expt.py --dataset rxrx1 --model densenet121 --algorithm ERM       --root_dir /root/Data/ --seed 512 --log_dir /root/Experiment/wilds/densenet121/ERM_rxrx1_2
+python examples/run_expt.py --dataset rxrx1 --model densenet121 --algorithm deepCORAL --root_dir /root/Data/ --seed 512 --log_dir /root/Experiment/wilds/densenet121/deepCORAL_rxrx1_2
+python examples/run_expt.py --dataset rxrx1 --model densenet121 --algorithm groupDRO  --root_dir /root/Data/ --seed 512 --log_dir /root/Experiment/wilds/densenet121/groupDRO_rxrx1_2
+python examples/run_expt.py --dataset rxrx1 --model densenet121 --algorithm IRM       --root_dir /root/Data/ --seed 512 --log_dir /root/Experiment/wilds/densenet121/IRM_rxrx1_2
 
-python examples/run_expt.py --dataset rxrx1 --model densenet121 --algorithm ERM       --root_dir /root/Data/ --seed 1024 --log_dir /root/Experiment/densenet121/ERM_rxrx1_3
-python examples/run_expt.py --dataset rxrx1 --model densenet121 --algorithm deepCORAL --root_dir /root/Data/ --seed 1024 --log_dir /root/Experiment/densenet121/deepCORAL_rxrx1_3
-python examples/run_expt.py --dataset rxrx1 --model densenet121 --algorithm groupDRO  --root_dir /root/Data/ --seed 1024 --log_dir /root/Experiment/densenet121/groupDRO_rxrx1_3
-python examples/run_expt.py --dataset rxrx1 --model densenet121 --algorithm IRM       --root_dir /root/Data/ --seed 1024 --log_dir /root/Experiment/densenet121/IRM_rxrx1_3
+python examples/run_expt.py --dataset rxrx1 --model densenet121 --algorithm ERM       --root_dir /root/Data/ --seed 1024 --log_dir /root/Experiment/wilds/densenet121/ERM_rxrx1_3
+python examples/run_expt.py --dataset rxrx1 --model densenet121 --algorithm deepCORAL --root_dir /root/Data/ --seed 1024 --log_dir /root/Experiment/wilds/densenet121/deepCORAL_rxrx1_3
+python examples/run_expt.py --dataset rxrx1 --model densenet121 --algorithm groupDRO  --root_dir /root/Data/ --seed 1024 --log_dir /root/Experiment/wilds/densenet121/groupDRO_rxrx1_3
+python examples/run_expt.py --dataset rxrx1 --model densenet121 --algorithm IRM       --root_dir /root/Data/ --seed 1024 --log_dir /root/Experiment/wilds/densenet121/IRM_rxrx1_3
--- run_rxrx1_res.sh
+++ run_rxrx1_res.sh
@@ -1,23 +1,23 @@
 #! /bin/bash
 
-python examples/run_expt.py --dataset rxrx1 --model resnet50 --algorithm ERM       --root_dir /root/Data/ --seed 0 --log_dir /root/Experiment/resnet50/ERM_rxrx1_0
-python examples/run_expt.py --dataset rxrx1 --model resnet50 --algorithm deepCORAL --root_dir /root/Data/ --seed 0 --log_dir /root/Experiment/resnet50/deepCORAL_rxrx1_0
-python examples/run_expt.py --dataset rxrx1 --model resnet50 --algorithm groupDRO  --root_dir /root/Data/ --seed 0 --log_dir /root/Experiment/resnet50/groupDRO_rxrx1_0
-python examples/run_expt.py --dataset rxrx1 --model resnet50 --algorithm IRM       --root_dir /root/Data/ --seed 0 --log_dir /root/Experiment/resnet50/IRM_rxrx1_0
+python examples/run_expt.py --dataset rxrx1 --model resnet50 --algorithm ERM       --root_dir /root/Data/ --seed 0 --log_dir /root/Experiment/wilds/resnet50/ERM_rxrx1_0
+# python examples/run_expt.py --dataset rxrx1 --model resnet50 --algorithm deepCORAL --root_dir /root/Data/ --seed 0 --log_dir /root/Experiment/wilds/resnet50/deepCORAL_rxrx1_0
+# python examples/run_expt.py --dataset rxrx1 --model resnet50 --algorithm groupDRO  --root_dir /root/Data/ --seed 0 --log_dir /root/Experiment/wilds/resnet50/groupDRO_rxrx1_0
+# python examples/run_expt.py --dataset rxrx1 --model resnet50 --algorithm IRM       --root_dir /root/Data/ --seed 0 --log_dir /root/Experiment/wilds/resnet50/IRM_rxrx1_0
 
-python examples/run_expt.py --dataset rxrx1 --model resnet50 --algorithm ERM       --root_dir /root/Data/ --seed 32 --log_dir /root/Experiment/resnet50/ERM_rxrx1_1
-python examples/run_expt.py --dataset rxrx1 --model resnet50 --algorithm deepCORAL --root_dir /root/Data/ --seed 32 --log_dir /root/Experiment/resnet50/deepCORAL_rxrx1_1
-python examples/run_expt.py --dataset rxrx1 --model resnet50 --algorithm groupDRO  --root_dir /root/Data/ --seed 32 --log_dir /root/Experiment/resnet50/groupDRO_rxrx1_1
-python examples/run_expt.py --dataset rxrx1 --model resnet50 --algorithm IRM       --root_dir /root/Data/ --seed 32 --log_dir /root/Experiment/resnet50/IRM_rxrx1_1
+python examples/run_expt.py --dataset rxrx1 --model resnet50 --algorithm ERM       --root_dir /root/Data/ --seed 32 --log_dir /root/Experiment/wilds/resnet50/ERM_rxrx1_1
+# python examples/run_expt.py --dataset rxrx1 --model resnet50 --algorithm deepCORAL --root_dir /root/Data/ --seed 32 --log_dir /root/Experiment/wilds/resnet50/deepCORAL_rxrx1_1
+# python examples/run_expt.py --dataset rxrx1 --model resnet50 --algorithm groupDRO  --root_dir /root/Data/ --seed 32 --log_dir /root/Experiment/wilds/resnet50/groupDRO_rxrx1_1
+# python examples/run_expt.py --dataset rxrx1 --model resnet50 --algorithm IRM       --root_dir /root/Data/ --seed 32 --log_dir /root/Experiment/wilds/resnet50/IRM_rxrx1_1
 
-python examples/run_expt.py --dataset rxrx1 --model resnet50 --algorithm ERM       --root_dir /root/Data/ --seed 512 --log_dir /root/Experiment/resnet50/ERM_rxrx1_2
-python examples/run_expt.py --dataset rxrx1 --model resnet50 --algorithm deepCORAL --root_dir /root/Data/ --seed 512 --log_dir /root/Experiment/resnet50/deepCORAL_rxrx1_2
-python examples/run_expt.py --dataset rxrx1 --model resnet50 --algorithm groupDRO  --root_dir /root/Data/ --seed 512 --log_dir /root/Experiment/resnet50/groupDRO_rxrx1_2
-python examples/run_expt.py --dataset rxrx1 --model resnet50 --algorithm IRM       --root_dir /root/Data/ --seed 512 --log_dir /root/Experiment/resnet50/IRM_rxrx1_2
+python examples/run_expt.py --dataset rxrx1 --model resnet50 --algorithm ERM       --root_dir /root/Data/ --seed 512 --log_dir /root/Experiment/wilds/resnet50/ERM_rxrx1_2
+# python examples/run_expt.py --dataset rxrx1 --model resnet50 --algorithm deepCORAL --root_dir /root/Data/ --seed 512 --log_dir /root/Experiment/wilds/resnet50/deepCORAL_rxrx1_2
+# python examples/run_expt.py --dataset rxrx1 --model resnet50 --algorithm groupDRO  --root_dir /root/Data/ --seed 512 --log_dir /root/Experiment/wilds/resnet50/groupDRO_rxrx1_2
+# python examples/run_expt.py --dataset rxrx1 --model resnet50 --algorithm IRM       --root_dir /root/Data/ --seed 512 --log_dir /root/Experiment/wilds/resnet50/IRM_rxrx1_2
 
-python examples/run_expt.py --dataset rxrx1 --model resnet50 --algorithm ERM       --root_dir /root/Data/ --seed 1024 --log_dir /root/Experiment/resnet50/ERM_rxrx1_3
-python examples/run_expt.py --dataset rxrx1 --model resnet50 --algorithm deepCORAL --root_dir /root/Data/ --seed 1024 --log_dir /root/Experiment/resnet50/deepCORAL_rxrx1_3
-python examples/run_expt.py --dataset rxrx1 --model resnet50 --algorithm groupDRO  --root_dir /root/Data/ --seed 1024 --log_dir /root/Experiment/resnet50/groupDRO_rxrx1_3
-python examples/run_expt.py --dataset rxrx1 --model resnet50 --algorithm IRM       --root_dir /root/Data/ --seed 1024 --log_dir /root/Experiment/resnet50/IRM_rxrx1_3
+python examples/run_expt.py --dataset rxrx1 --model resnet50 --algorithm ERM       --root_dir /root/Data/ --seed 1024 --log_dir /root/Experiment/wilds/resnet50/ERM_rxrx1_3
+# python examples/run_expt.py --dataset rxrx1 --model resnet50 --algorithm deepCORAL --root_dir /root/Data/ --seed 1024 --log_dir /root/Experiment/wilds/resnet50/deepCORAL_rxrx1_3
+# python examples/run_expt.py --dataset rxrx1 --model resnet50 --algorithm groupDRO  --root_dir /root/Data/ --seed 1024 --log_dir /root/Experiment/wilds/resnet50/groupDRO_rxrx1_3
+# python examples/run_expt.py --dataset rxrx1 --model resnet50 --algorithm IRM       --root_dir /root/Data/ --seed 1024 --log_dir /root/Experiment/wilds/resnet50/IRM_rxrx1_3
 
 
